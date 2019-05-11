d29893b672f233d2230d72a1c7e4b9e45f873590
Steven Espinal <stevenjesusespinal@gmail.com>
2019-05-11 15:45:45 -0400

    Files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+// import NavBar from './components/layout/NavBar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Index from './components/layout/Index';
+import {Provider} from './context';
+import Lyrics from './components/tracks/Lyrics';
+class App extends Component {
+  render() {
+    return (
+      <Provider>
+      <Router>
+        <React.Fragment>
+          {/* <NavBar /> */}
+          <div className="container mt-4">
+            <Switch>
+              <Route exact path="/" component={Index} />
+              <Route exact path="/lyrics/track/:id" component={Lyrics} />
+            </Switch>
+          </div>
+        </React.Fragment>
+      </Router>
+      </Provider>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/layout/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-dark bg-dark mb-5">
+        <span className="navbar-brand mb-0 h1 mx-auto">Lyric Finder</span>
+      </nav>
+    </div>
+  );
+}
+
+export default NavBar;
